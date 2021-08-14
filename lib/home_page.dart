a4c12b9a8d955c2b34afde697238ab0a18e1d0c2
Fuad-SM <rzaki804@gmail.com>
2021-08-14 21:11:57 +0800

    menambahkan description dan listview untuk scroll

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,8 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
         children: [
           Image.asset('assets/images/Subaru.png'),
           SizedBox(
@@ -106,6 +105,13 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
+          Padding(
+              padding: EdgeInsets.only(left: 30, top: 30, right: 30, bottom: 0),
+              child: Text(
+                "Asahi's life in reality ended simultaneously when she died playing Union, a world-renowned online RPG. The members of Subaru went separate ways when Union shut down due to Asahi's death. Six years later, Haruto logs onto a renewed version of Re'Union and reunites with Asahi who had died. Is it really her or just an electronic ghost? The former team members reunite and enter the mysterious and deadly online game to find out. In 2034, in the globally popular MMORPG Union, there was once a top group of legends named Subaru, made of six elementary school friends. However, once one of their members died of a heart attack presumably brought on from dying in the game, Union shut down the game. Six years later, a new game called Re'Union is launched, with similar mechanics, and when Haruto, one of Subaru's original members, meets Asahi, his partner who died six years before, the members of Subaru gather once again to uncover the mystery behind it. Seven Senses of the Reunion (Japanese: 七星のスバル, Hepburn: Shichisei no Subaru, lit. Subaru of the Seven Stars) is a Japanese light novel series written by Noritake Tao and illustrated by Booota. Eight volumes have been published by Shogakukan since August 2015 under their Gagaga Bunko imprint. An anime television series adaptation by Lerche aired from July to September 2018. \n \nSeri ini bercerita mengenai party legendaris bernama Subaru dalam game MMORPG populer, Union. Party tersebut terdiri dari sekumpulan teman masa kecil dan anak SD, dan berhasil melampaui batasan game dengan kemampuan mereka. Namun, sebuah insiden yang berujung kematian membuat Union menutup game tersebut dan kumpulan teman tersebut pun berpisah. 6 tahun kemudian, murid SMA Haruto bermain game ‘Reunion’ dan bertemu kembali dengan seorang gadis. Asahi, seorang anggota party Subaru yang seharusnya sudah meninggal 6 tahun lalu. Hantu? Atau…? Seri ini ditulis oleh Noritake Tao dengan ilustrasi yang digambar oleh booota, dan telah diterbitkan sejak bulan Agustus 2015. Seri ini saat ini memiliki 5 edisi, dengan edisi keenam direncanakan rilis tanggal 20 September. ",
+                style: TextStyle(fontSize: 12),
+                textAlign: TextAlign.justify,
+              )),
         ],
       )),
     );
