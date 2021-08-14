4e0b67e9b947ddc10752576e8fb0ecf2c731204d
Fuad-SM <rzaki804@gmail.com>
2021-08-14 17:46:43 +0800

    menambahkan iconbutton di bagian tengah

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,40 +13,99 @@ class HomePage extends StatelessWidget {
             height: 30,
           ),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Shichisei no Subaru',
-                        style: TextStyle(fontWeight: FontWeight.w700),
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shichisei no Subaru',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Seven Senses of the Re\'Union',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12,
                       ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        'Seven Senses of the Re\'Union',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12,
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.favorite,
-                        color: Color(0xffFFB800),
-                      ),
-                      Text('4.2')
-                    ],
-                  )
-                ],
-              )),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.favorite,
+                          color: Color(0xffFFB800),
+                        )),
+                    Text('4.2'),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30, top: 20, right: 30, bottom: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.facebook_sharp,
+                          color: Color(0xff00A3FF),
+                          size: 18,
+                        )),
+                    Text(
+                      'Facebook',
+                      style: TextStyle(color: Color(0xff00A3FF), fontSize: 12),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.share,
+                          color: Color(0xff00A3FF),
+                          size: 18,
+                        )),
+                    Text(
+                      'Share',
+                      style: TextStyle(color: Color(0xff00A3FF), fontSize: 12),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.help,
+                          color: Color(0xff00A3FF),
+                          size: 18,
+                        )),
+                    Text(
+                      'Help',
+                      style: TextStyle(color: Color(0xff00A3FF), fontSize: 12),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       )),
     );
