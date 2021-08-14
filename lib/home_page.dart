b315109d9a6c629f7f478337a90d27210ad274fe
Fuad-SM <rzaki804@gmail.com>
2021-08-14 16:33:53 +0800

    penambahan column dan text untuk judul/subjudul

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Image.asset('assets/images/Subaru.png')),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset('assets/images/Subaru.png'),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Shichisei no Subaru',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        'Seven Senses of the Re\'Union',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )),
+        ],
+      )),
     );
   }
 }
